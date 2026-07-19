183d04da0a44fd151ef7b150f5d3947ef1b5e280
Shifa <shifakhan11045@gmail.com>
2026-07-19 22:33:58 +0530

    Linear Search added

---
+++ Searching/LinearSearching.java
@@ -0,0 +1,21 @@
+class LinearSearching{
+    public static void main(String[] args)
+    {
+        int arr[] = {10,20,30,40,50};
+        int element = 30;
+        boolean found = false;
+        for(int i = 0 ; i < arr.length ; i++)
+        {
+            if(arr[i] == element)
+            {
+                System.out.println("Element found at index = "+ i);
+                found = true;
+                break;
+            }
+        }
+        if(!found)
+        {
+            System.out.println("Element not found!!");
+        }
+    }
+}
